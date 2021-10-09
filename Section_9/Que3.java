e16316a6a835f2bd75fb09856cf0099f57794ceb
Harshal Deshattiwar <75268366+Harshal-Deshattiwar@users.noreply.github.com>
2021-10-10 00:47:47 +0530

    done
    3. Write a program to calculate the product of two integers using recursion? (Multiplication & Division operators are not allowed)

--- Section_9/Que3.java
+++ Section_9/Que3.java
@@ -1,4 +1,5 @@
-package com.Section_9;
+//3. Write a program to calculate the product of two integers using recursion? (Multiplication & Division operators are not allowed)
+
 
 import java.util.Scanner;
 
@@ -8,21 +9,26 @@ public class Que3 {
 		if (num2 > 0) {
 
 			product += num1;
-			Que3.number(num1, num2 -1,product);
+			Que3.number(num1, num2 -1, product);
 		} else
-			System.out.println("product is "+product);
+			System.out.println("product is "+ product);
 		;
 
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Enter the first number : ");
 		int N1 = sc.nextInt();
+		
 		System.out.println("Enter the second number : ");
 		int N2 = sc.nextInt();
+		
 		int N3=0;
+		
 		Que3.number(N1,N2,N3);
+		
 		sc.close();
 
 	}
