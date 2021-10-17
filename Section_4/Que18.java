e4aee86882e1f488bae74f2900da6aa7ec4c5070
Harshal Deshattiwar <75268366+Harshal-Deshattiwar@users.noreply.github.com>
2021-10-17 18:29:32 +0530

    done

---
+++ Section_4/Que18.java
@@ -0,0 +1,25 @@
+/*
+18. Write a program to find the length of a string without using the library function?
+*/
+
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		String str = "Hello";
+		int len=0;
+		if (str == "" || str == null) {System.out.println("Length: " + len ); return;} 
+		char c=str.charAt(len);
+		while(c!='\0') {
+			len++;
+			try {
+				c=str.charAt(len);
+			} catch(StringIndexOutOfBoundsException e) {
+				break;
+			}
+		}
+		System.out.println("Length: " + len);	
+	}		
+}
